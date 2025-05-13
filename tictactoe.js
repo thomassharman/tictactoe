2698dbc403736356607d2b08d52cc6eb4baa0846
cocosharms14 <sharmanthomas.james@gmail.com>
2025-05-13 22:40:10 +0100

    game functionality branch

--- tictactoe.js
+++ tictactoe.js
@@ -18,14 +18,37 @@ playerX.addEventListener ('click', () => {currentPlayer = 'X'
 
 boxes.forEach((box) => {box.addEventListener('click',  function() {
     console.log(this.classList) //check it has successfully selected the box
+    
     if (this.textContent === '' && currentPlayer != 'none') {
-        this.textContent = currentPlayer
+        const boxIndex = parseInt(this.classList[1]) //get the second class set of the box and use it as the index
+        console.log('BI:', boxIndex)
+        const playerMove = Gameboard.cells(boxIndex, currentPlayer) //pass the second class of the object clicked as the index of the item in the gameboard array and set the value to the current player X or O
+        console.log('Gameboard:', Gameboard.board)
+
+        if (playerMove) {
+  this.textContent = currentPlayer
+
       if (currentPlayer === 'X') {
         this.style.color = 'white';
-        this.style.backgroundColor = 'rgb(163, 64, 64)';}
-             else if (currentPlayer === 'O') {
+        this.style.backgroundColor = 'rgb(163, 64, 64)'
+        Gameboard.winner(currentPlayer)
+        // checkWins(currentPlayer)
+        currentPlayer = 'O'; //switch current player BEFORE auto-switching
+        showCurrentPlayer()
+        }
+        
+
+        else if (currentPlayer === 'O') {
         this.style.color = 'white';
-        this.style.backgroundColor = 'rgb(53, 116, 66';}
+        this.style.backgroundColor = 'rgb(53, 116, 66'
+        Gameboard.winner(currentPlayer)
+        // checkWins(currentPlayer)
+        currentPlayer = 'X';
+        showCurrentPlayer()
+        }
+
+        }
+      
     }
 })})
 
@@ -36,14 +59,52 @@ if(currentPlayer === 'X') {
     playerO.style.backgroundColor = 'rgba(255, 255, 255, 0)'
     playerX.style.color = 'white'
     playerX.style.backgroundColor = 'rgb(163, 64, 64)'
-    console.log('text is red')
+    console.log(Gameboard)
 }
 else if(currentPlayer === 'O') {
     playerX.style.color = 'black'
     playerX.style.backgroundColor = 'rgba(255, 255, 255, 0)'
     playerO.style.color = 'white'
     playerO.style.backgroundColor = 'rgb(53, 116, 66)'
-    console.log('text is blue')
-    playerDOMText.style.backgroundColor = 'rgba(128, 210, 237, 0.312)'
+    console.log(Gameboard)
+
 }
 }
+
+//gameboard array inside gameboard object
+
+const Gameboard = {
+    board: ['','','','','','','','',''],
+    
+    getBoard: function() {
+        return this.board
+    },
+
+    cells: function(index, player) {
+        if (this.board[index] === '') {
+            this.board[index] = player
+            return true
+}
+            return false
+    },
+    winner: function(player) {
+
+        const wins = [
+                [0,1,2], [3,4,5], [6,7,8],//columns
+                [0,3,6], [1,4,7], [2,5,8], //rows
+                [0,4,8], [6,4,2]//diagonals
+            ]
+
+        for (i = 0; i < wins.length; i++) {
+    const check = wins[i]
+    const a = this.board[check[0]]
+    const b = this.board[check[1]]
+    const c = this.board[check[2]]
+       
+    if (a === player && b === player && c === player) {
+        console.log('winner:', player)
+        return true
+    }
+
+
+}}}
